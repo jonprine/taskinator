97c16adca83cc5930534c746e214713a18764ae8
Vox Humana <jonprine@gmail.com>
2020-06-29 13:26:19 -0500

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -296,6 +296,17 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+  var savedTasks = localStorage.getItem('tasks');
+  if (!savedTasks) {
+      return false;
+  }
+  savedTasks = JSON.parse(savedTasks);
+  for (var i = 0; i < savedTasks.length; i++) {
+      createTaskEl(savedTasks[i]);
+  }
+}
+
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
